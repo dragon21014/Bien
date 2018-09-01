27b245956aceecc5616c1050995cafec86471960
Kym Begg <kymbegg@hotmail.com>
2018-09-01 18:59:17 +0930

    added reviews to the site

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,73 @@
+class ReviewsController < ApplicationController
+
+def index
+  #this is our list page for our reviews
+
+@number = rand(100)
+
+
+@reviews = Review.all
+
+end
+
+def new
+# the form for adding a new review
+  @review = Review.new
+
+end
+
+def create
+#take info from the form and add it to the database
+@review = Review.new(form_params)
+
+#save this to the database
+
+@review.save
+
+#redirect back to the homepage
+
+redirect_to  root_path
+end
+
+def show
+#indvidual review page
+
+@review = Review.find(params[:id])
+
+end
+
+def destroy
+  #find the indvidiaul review
+  @review = Review.find(params[:id])
+
+#destroy
+@review.destroy
+
+#redirect to the home page
+redirect_to root_path
+
+end
+
+def edit
+  #find the indvidual review (to edit)
+  @review = Review.find(params[:id])
+
+end
+
+def update
+  #find the indvidaul review
+  @review = Review.find(params[:id])
+
+  #Update with the new info form the form
+  @review.update(form_params)
+
+#redirect somewhere new
+redirect_to review_path(@review)
+
+end
+
+def form_params
+  params.require(:review).permit(:title, :body, :score)
+
+end
+end
