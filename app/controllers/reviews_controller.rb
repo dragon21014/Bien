0e7794d6936127a568a3c8fa64f02240b95bf9c6
Rik Lomas <lomas.rik@gmail.com>
2018-09-11 10:11:01 -0400

    Indented code and added `end` at the end

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,108 +1,93 @@
 class ReviewsController < ApplicationController
 
-def index
-#this is our list page for our reviews
+  def index
+    #this is our list page for our reviews
 
-  @price = params[:price]
-  @cuisine = params[:cuisine]
-  @location = params[:location]
+    @price = params[:price]
+    @cuisine = params[:cuisine]
+    @location = params[:location]
 
-#start with all the reviews
+    #start with all the reviews
 
-@reviews = Review.all
+    @reviews = Review.all
 
-#filtering by price
-if @price.present?
-  @reviews = @reviews.where(price: @price)
-end
+    #filtering by price
+    if @price.present?
+      @reviews = @reviews.where(price: @price)
+    end
 
-#filter by cuisine
+    #filter by cuisine
 
-if @cuisine.present?
-  @reviews = @reviews.where(cuisine: @cuisine)
-end
-
-
-#Searh near the location
-
-if @location.present?
-  @reviews = @reviews.near(@location)
-end
-
-end
-
-def new
-  # the form for adding a new review
-  @review = Review.new
-end
-
-def create
-#take info from the form and add it to the database
-@review = Review.new(form_params)
-
-#we want to check if the model can be saved
-#if it is, we're going to the homepage again
-#if it isn't, show the new form
-
-if @review.save
-  redirect_to root_path
-else
-  #show the view for new.html.erb
-  render "new"
-end
-
-end
+    if @cuisine.present?
+      @reviews = @reviews.where(cuisine: @cuisine)
+    end
 
 
+    #Searh near the location
 
-def show
-#indvidual review page
+    if @location.present?
+      @reviews = @reviews.near(@location)
+    end
+  end
 
-@review = Review.find(params[:id])
+  def new
+    # the form for adding a new review
+    @review = Review.new
+  end
 
-end
+  def create
+    #take info from the form and add it to the database
+    @review = Review.new(form_params)
 
-def destroy
-  #find the indvidiaul review
-  @review = Review.find(params[:id])
+    #we want to check if the model can be saved
+    #if it is, we're going to the homepage again
+    #if it isn't, show the new form
 
-  #destroy
-  @review.destroy
+    if @review.save
+      redirect_to root_path
+    else
+      #show the view for new.html.erb
+      render "new"
+    end
+  end
 
-  #redirect to the home page
-  redirect_to root_path
+  def show
+    #indvidual review page
+    @review = Review.find(params[:id])
+  end
 
-end
+  def destroy
+    #find the indvidiaul review
+    @review = Review.find(params[:id])
 
-def edit
-  #find the indvidual review (to edit)
-  @review = Review.find(params[:id])
+    #destroy
+    @review.destroy
 
-end
+    #redirect to the home page
+    redirect_to root_path
+  end
 
+  def edit
+    #find the indvidual review (to edit)
+    @review = Review.find(params[:id])
+  end
 
+  def update
+    #find the indvidaul review
+    @review = Review.find(params[:id])
 
-def update
-  #find the indvidaul review
-  @review = Review.find(params[:id])
+    #Update with the new info from the form
+    if @review.update(form_params)
 
-  #Update with the new info from the form
-  if @review.update(form_params)
+      #redirect somewhere new
+      redirect_to review_path(@review)
+    else
+      render "edit"
+    end
+  end
 
-  #redirect somewhere new
-  redirect_to review_path(@review)
-else
-
-render "edit"
-
-end
-
-end
-
-
-
-def form_params
-  params.require(:review).permit(:title, :restaurant, :body, :score,
+  def form_params
+    params.require(:review).permit(:title, :restaurant, :body, :score,
      :ambiance, :cuisine, :price, :address)
-
-   end
+  end
+end
